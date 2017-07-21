304264ecb3f057a40a2875188405dd03e0420677
Michael Nissen <m9ssen@gmail.com>
2017-07-21 18:08:25 +0200

    Abstracted App into seperate component and inject in index.ios and index.android

--- index.ios.js
+++ index.ios.js
@@ -13,56 +13,14 @@ import {
 } from 'react-native';
 
 import Camera from 'react-native-camera'
+import App from './App.js'
 
 export default class cameraTutorial extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam
-          }}
-          style={styles.view}
-          aspect={Camera.constants.Aspect.fill}>
-            <Text 
-            style={styles.capture} 
-            onPress={this.takePicture.bind(this)}>
-              [CAPTURE_IMAGE]
-            </Text>
-        </Camera>
-      </View>
+      <App />
     );
   }
-
-  takePicture() {
-    const options = {}
-
-    this.camera.capture({metadata: options}).then((data) => {
-      console.log(data)
-    }).catch((error) => {
-      console.log(error)
-    })
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  view: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: 'steelblue',
-    borderRadius: 10,
-    color: 'red',
-    padding: 15,
-    margin: 45
-  }
-});
 
 AppRegistry.registerComponent('cameraTutorial', () => cameraTutorial);
