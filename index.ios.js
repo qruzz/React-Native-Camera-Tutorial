bb633c08dafb953d276dbe91bfeca7a015e94244
Michael Nissen <m9ssen@gmail.com>
2017-07-21 17:11:42 +0200

    Implemented iOS

---
+++ index.ios.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Camera from 'react-native-camera'
+
+export default class cameraTutorial extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam
+          }}
+          style={styles.view}
+          aspect={Camera.constants.Aspect.fill}>
+            <Text 
+            style={styles.capture} 
+            onPress={this.takePicture.bind(this)}>
+              [CAPTURE_IMAGE]
+            </Text>
+        </Camera>
+      </View>
+    );
+  }
+
+  takePicture() {
+    const options = {}
+
+    this.camera.capture({metadata: options}).then((data) => {
+      console.log(data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  view: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: 'steelblue',
+    borderRadius: 10,
+    color: 'red',
+    padding: 15,
+    margin: 45
+  }
+});
+
+AppRegistry.registerComponent('cameraTutorial', () => cameraTutorial);
